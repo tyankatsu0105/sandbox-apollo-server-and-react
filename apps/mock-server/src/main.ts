1a3c008012a2f55b51f2d5fb13565420917dd1ae
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-03-03 23:34:50 +0900

    mock-server

--- apps/mock-server/src/main.ts
+++ apps/mock-server/src/main.ts
@@ -1,18 +1,44 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
+import * as ApolloServerExpress from 'apollo-server-express';
+import * as VoyagerMiddleware from 'graphql-voyager/middleware';
+import * as Fs from 'fs';
+import * as Path from 'path';
+import * as boxen from 'boxen';
+import * as Resolvers from './app/resolvers';
+
+const ENDPOINT_VOYAGER = '/voyager' as const;
+
+const typeDefs = ApolloServerExpress.gql(
+  Fs.readFileSync(Path.resolve('apps/graphql/schema.graphql'), 'utf8')
+);
+
+const server = new ApolloServerExpress.ApolloServer({
+  typeDefs,
+  resolvers: Resolvers.resolvers,
+});
+const ENDPOINT_GRAPHQL = server.graphqlPath;
 
 const app = express();
+app.use(
+  ENDPOINT_VOYAGER,
+  VoyagerMiddleware.express({ endpointUrl: ENDPOINT_GRAPHQL })
+);
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to mock-server!' });
-});
+server.applyMiddleware({ app });
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+app.listen({ port: 4000 }, () => {
+  console.log(
+    boxen(
+      `
+🚀 Server ready at http://localhost:4000${server.graphqlPath}
+
+🚀 Voyager ready at http://localhost:4000${ENDPOINT_VOYAGER}
+    `,
+      {
+        padding: 1,
+        borderStyle: 'round',
+        borderColor: 'greenBright',
+      }
+    )
+  );
 });
-server.on('error', console.error);
