e156f9c786a65afc6bd24c60810873dc78d50276
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-04-05 22:34:23 +0900

    usercard

--- apps/client/src/app/ui/design/components/card/presentational.tsx
+++ apps/client/src/app/ui/design/components/card/presentational.tsx
@@ -2,11 +2,25 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 // ------------------------------------
+// Props
+// ------------------------------------
+
+type Props = {
+  body?: React.ReactNode;
+  footer?: React.ReactNode;
+  header?: React.ReactNode;
+};
+
+// ------------------------------------
 // Component
 // ------------------------------------
 
-export const Component: React.FC = (props) => (
-  <StyledWrap>{props.children}</StyledWrap>
+export const Component = (props: Props) => (
+  <StyledWrap>
+    {props.header && <StyledHeader>{props.header}</StyledHeader>}
+    {props.body && <StyledBody>{props.body}</StyledBody>}
+    {props.footer && <StyledFooter>{props.footer}</StyledFooter>}
+  </StyledWrap>
 );
 
 // ------------------------------------
@@ -15,4 +29,17 @@ export const Component: React.FC = (props) => (
 
 const StyledWrap = styled.div`
   box-shadow: 0 0 6px #ccc;
+`;
+
+const StyledHeader = styled.div`
+  padding: 12px;
+  border-bottom: 1px solid #000;
+`;
+
+const StyledBody = styled.div`
+  padding: 12px;
+`;
+
+const StyledFooter = styled.div`
+  padding: 12px;
 `;
--- apps/client/src/app/ui/design/recipies/user-card/presentational.tsx
+++ apps/client/src/app/ui/design/recipies/user-card/presentational.tsx
@@ -16,5 +16,39 @@ type Props = {
 // ------------------------------------
 
 export const Component = (props: Props) => (
-  <Components.Card.Component>{props.user.createdAt}</Components.Card.Component>
+  <Components.Card.Component
+    body={
+      <div>
+        <ul>
+          <li>誕生日{props.user.birthDay}</li>
+          <li>
+            <ul>
+              book
+              {props.user.favorites?.books?.map((book) => (
+                <li key={book}>{book}</li>
+              ))}
+            </ul>
+          </li>
+          <li>
+            <ul>
+              movie
+              {props.user.favorites?.movies?.map((movie) => (
+                <li key={movie}>{movie}</li>
+              ))}
+            </ul>
+          </li>
+          <li>
+            <ul>
+              music
+              {props.user.favorites?.musics?.map((music) => (
+                <li key={music}>{music}</li>
+              ))}
+            </ul>
+          </li>
+        </ul>
+      </div>
+    }
+    footer={<div>ユーザー作成日{props.user.createdAt}</div>}
+    header={<div>{props.user.name}</div>}
+  />
 );
